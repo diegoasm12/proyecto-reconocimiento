cb22ae3d4b4f44c5ad3e3e9797ad11f41c53a003
diegoasm12 <102439978+diegoasm12@users.noreply.github.com>
2023-03-16 09:22:49 -0300

    Update cotejo.php

--- cotejo.php
+++ cotejo.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="asset/css/css.css">
+  <script src="asset/js/scrip.js"></script>
 
 </head>
 
