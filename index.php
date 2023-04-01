d36beab8f21ce9973c942423f7cc1f4559d8a124
diegoasm12 <102439978+diegoasm12@users.noreply.github.com>
2023-04-01 12:13:05 -0300

    hice muchas cosas

--- index.php
+++ index.php
@@ -6,37 +6,73 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reconocimiente Oficial</title>
-    <link rel="stylesheet" href="asset/css/css.css">
+    <link rel="stylesheet" href="asset/css/main.css">
     <link rel="shortcut icon" href="asset/img/logo-gobierno.png" type="image/x-icon">
 </head>
+<div class="div_gris">
+    <div class="div_blanco">
+        <div><img class="imagen_edificio" src="asset/img/edificio.PNG" alt=""> </div>
+        <div><img class="imagen_secretaria" src="asset/img/logo-secretaria.jpg" alt=""> </div>
+        <div>
+            <h1 class="texto_titulo">Reconocimiento oficial 2023</h1>
+        </div>
+        <div class="contenedor1">
+            <h2>"INSTRUCTIVO SOBRE PROCEDIMIENTO ADMINISTRATIVO DE SOLICITUDES DE RECONOCIMIENTO OFICIAL 2023"</h2>
+        </div>
+        <div class="div_info_secre">
+            <table>
+                <tr>
+                    <th>Jefe de Gabinete</th>
+                    <td><a href="mailto:alberto.inzulza@mineduc.cl">alberto.inzulza@mineduc.cl</a></td>
+                </tr>
+                <tr>
+                    <th>Medio de envío de Solicitudes</th>
+                    <td><a href="mailto:secreduc.maule@mineduc.cl">secreduc.maule@mineduc.cl</td>
+                </tr>
+                <tr>
+                    <th>Horario de presentación de Solicitudes</th>
+                    <td>Papel en oficina de partes Secreduc maule: de Lunes a Viernes de 9 a 13:30 hrs.</td>
+                </tr>
+                <tr>
+                    <th>Cumplimiento de la normativa Educacional Vigente</th>
+                    <td>El solicitante además de cumplir con este Instructivo debe cumplir con la totalidad de la
+                        normativa educacional vigente.</td>
+                </tr>
+                <tr>
+                    <th>Siglas</th>
+                    <td>NA: No Aplica &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        PEI: Proyecto Educativo Institucional</br>
+                        E.E.: Establecimiento Educacional &nbsp;&nbsp;&nbsp;
+                        TP:Técnico Profesional </br>
+                        JEC: Jornada Escolar Completa &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        HC: Humanístico Científico </br>
+                        Ed.: Educación &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        CBR:Conservador de Bienes Raíces.</td>
+                </tr>
+                <tr>
+                    <th>Contactos</th>
+                    <td>
+                    <a href="mailto:cristian.lopez@mineduc.cl">Coordinador: cristian.lopez@mineduc.cl</br>
+                    <a href="mailto:natalia.gonzalezb@mineduc.cl">jurídica:natalia.gonzalezb@mineduc.cl<br>
+                    <a href="mailto:jaime.rojas@mineduc.cl">Infraestructura: jaime.rojas@mineduc.cl,paulo.lagos@mineduc.cl<br>
+                    <a href="mailto:Luis.gaete@mineduc.cl">Coordinador R.O. de educación Parvularios: Luis.gaete@mineduc.cl</td>
+                </tr>
+            </table>
 
-<body>
-    <script>
-        document.body.style.zoom = "80%";
-      </script>
-    <div class="div_gris">
-        <div class="div_blanco">
-            <div><img class="imagen_edificio" src="asset/img/edificio.PNG" alt=""> </div>
-            <div><img class="imagen_secretaria" src="asset/img/logo-secretaria.jpg" alt=""> </div>
-            <div>
-                <h1 class="texto_titulo">Reconocimiento oficial 2023</h1>
-            </div>
-            <div class="contenedor1">
-                <h2 class="texto_sub">"INSTRUCTIVO SOBRE PROCEDIMIENTO ADMINISTRATIVO DE SOLICITUDES DE RECONOCIMIENTO
-                    OFICIAL 2023"</h2>
-            </div>
-            <div class="contenedor2"> <img src="asset/img/Captura.PNG" alt=""></div>
-            <div class="contenedor3"><button class="btn"><a class="negro" href="cotejo.php">Ver Tipos De Solicitudes</a></button>
-                <div class="contenedor5">
-                    <h2 class="texto_sub">"Importante: Este instructivo es solo una guía, siendo responsabilidad del
-                        sostenedor informarse de toda la normativa vigente, así como de las modificaciones que puedan
-                        surgir."</h2>
-                </div>
+        </div>
+        <div class="contenedor3"><button class="btn"><a class="negro" href="cotejo.php">Ver Tipos De
+                    Solicitudes</a></button>
+
+            
+                    <div class="contenedor5">
+                <h2 class="texto_sub">"Importante: Este instructivo es solo una guía, siendo responsabilidad del
+                    sostenedor informarse de toda la normativa vigente."</h2>
             </div>
         </div>
-
-
     </div>
+
+
+</div>
 
 
 </body>
